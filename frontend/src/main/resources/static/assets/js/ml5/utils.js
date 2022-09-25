77d1c86350957810725a6cc0a66c24edfac94cac
Ze Guru <mabura.zeguru@gmail.com>
2022-09-25 11:02:49 +0300

    Fine tune detection and draw speed

--- frontend/src/main/resources/static/assets/js/ml5/utils.js
+++ frontend/src/main/resources/static/assets/js/ml5/utils.js
@@ -1,21 +1,37 @@
-function showImage(incidentId) {
-    
-    let url = 'http://localhost:8080/classif-ai/crud/detection/domain/' + incidentId;
-    fetch(url)
-        .then((response) => {
-            return response.json();
-            })
-        .then((data) => {
-            let det = data;
-            document.getElementById("snapshot").src = det.base64;
+    function showImage(incidentId) {
+        
+        let url = 'http://localhost:8080/classif-ai/crud/detection/domain/' + incidentId;
 
-            let narrative = "<h5 class=\"card-title\">" + det.createdAt + "</h5><p class=\"card-text\">" + det.label + " ...</p>";
-            document.getElementById("snapshot-narrative").innerHTML = narrative;
+        fetch(url)
+            .then((response) => {
+                return response.json();
+                })
+            .then((data) => {
+                let det = data;
+                document.getElementById("snapshot").src = det.base64;
 
-            })
-        .catch((error) => {
-            console.log(error);
-            });
+                let narrative = "<h5 class=\"card-title\">" + det.createdAt + "</h5><p class=\"card-text\">" + det.label + " ...</p><a href=\"#\" class=\"btn btn-primary\">More insights</a>";
+
+                document.getElementById("snapshot-narrative").innerHTML = narrative;
+
+                })
+            .catch((error) => {
+                console.log(error);
+                });
+
+        }
 
 
-    }
+
+    async function apiPostRequest(url,options) {
+        const response = await fetch(
+            url,
+            options
+            );
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+            }
+        const data = await response.json();
+        return data;
+        }
+
