364e929a44abe4e4780d946c30b6583dc5c839ee
Ze Guru <mabura.zeguru@gmail.com>
2022-12-05 13:19:53 +0300

    Caching assets. TODO cache the templates too

--- frontend/src/main/resources/static/sw.js
+++ frontend/src/main/resources/static/sw.js
@@ -1,12 +1,24 @@
 
 var cacheName = 'classif-ai-pwa';
 var filesToCache = [
-  //'/',
-  //'/classif-ai',
+  // '/',
+  "/classif-ai/",
+  // '/classif-ai/templates/index.ftlh',
+  "/classif-ai/assets/media/svg/brand-logos/tower.svg",
+  "/classif-ai/assets/css/style.bundle.css",
+
+  // '/settings.ftlh',
+  // "/settings/",
+  "/classif-ai/settings/",
+  // '/classif-ai/templates/settings.ftlh',
+  "/classif-ai/assets/js/ml5/utils.js",
+  "/classif-ai/assets/js/ml5/events.js",
+
   // '/index.html',
   // '/simple/index.html',
   // '/simple/css/style.css',
-  // '/simple/js/main.js'
+  '/classif-ai/favicon.ico',
+  '/classif-ai/assets/js/main.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
